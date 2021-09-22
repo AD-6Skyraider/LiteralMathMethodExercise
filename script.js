ff890b50ebcf8733356b9c811430ecf686231ee5
Don Vest <tsukasasan@hotmail.com>
2021-09-22 16:02:27 -0500

    Created and mostly complted LiteralMathMethodExercise

---
+++ script.js
@@ -0,0 +1,40 @@
+// 1a.b.c.
+let warmHugs = "Hi, I\'m Olaf and I like warm hugs.";
+console.log(warmHugs.toUpperCase());
+console.log(warmHugs.replace("like", "love"));
+
+// 2a.b.
+let beenImpaled = "Oh, look at that. I've been impaled.";
+console.log(beenImpaled.slice(18, 36));
+
+// 3a.b.
+const dotDotDot = "...";
+let skullBones = "I don\'t have a skull ${ } or bones.";
+skullBones = `I don\'t have a skull ${dotDotDot} or bones.`;
+console.log(skullBones);
+
+// 4
+Math.PI
+console.log(Math.PI);
+
+// 5
+let randomNumber = Math.random();
+randomNumber ++;
+console.log(randomNumber);
+
+// Bonus 1 (6)
+let horribleSong = " " + "LET IT GO!"
+console.log(horribleSong.repeat(3));
+
+// Bonus 2 (7a.b.)
+let reindeers = "Reindeers are better than people."
+console.log(reindeers.replace(" ", "_"));
+
+// Bonus 8 (3)
+Math.SQURT2
+console.log(Math.SQRT2);
+
+// Bonus 9 (4)
+let newRandomNumber = Math.random();
+I have no blasted idea.()
+
